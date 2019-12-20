dd7637bff01bd35c0dd05de04cb49e8a69a5d088
Mona Lisa <1579011033@qq.com>
2019-12-20 13:48:41 +0800

    aaa01

--- src/main/java/cn/xxct/oa_element/controller/wildfireController.java
+++ src/main/java/cn/xxct/oa_element/controller/wildfireController.java
@@ -1,4 +1,31 @@
 package cn.xxct.oa_element.controller;
 
+import cn.xxct.oa_element.service.ArchivesService;
+import cn.xxct.oa_element.service.DeliveryService;
+import cn.xxct.oa_element.service.Dynamic_MessageService;
+import cn.xxct.oa_element.utils.ResultEntity;
+import org.activiti.engine.impl.util.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class wildfireController {
+    @Autowired
+    private DeliveryService deliveryService;
+    @Autowired
+    private Dynamic_MessageService dynamic_messageService;
+    @Autowired
+    private ArchivesService archivesService;
+
+
+
+
+    @RequestMapping("/add")
+    public ResultEntity a(@RequestBody JSONObject jsonObject){
+        
+        return null;
+    }
+
 }
